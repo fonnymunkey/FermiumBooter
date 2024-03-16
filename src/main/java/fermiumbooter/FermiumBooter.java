06197cfa1d51656f0ac386b9c3f416f989b44f29
fonnymunkey <fonnymunkey@gmail.com>
2024-03-16 19:50:38 -0400

    Bump version 1.0.1

--- src/main/java/fermiumbooter/FermiumBooter.java
+++ src/main/java/fermiumbooter/FermiumBooter.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.Mod.Instance;
 public class FermiumBooter
 {
     public static final String MODID = "fermiumbooter";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "FermiumBooter";
 	
 	@Instance(MODID)
